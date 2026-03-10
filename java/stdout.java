385b3428da2c9bcd651fb23d87e55934147aa4ba
sanakar-03 <158248117+sanakar-03@users.noreply.github.com>
2026-03-10 09:59:13 +0530

    Add files via upload

---
+++ java/stdout.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a;
+        for(int i = 0; i < 3; i++) {
+            a = scan.nextInt();
+            System.out.println(a);
+        }
+
+    }
+}
